002650054a4dd9406d97114f22b20f1e2d436888
Marturelo007 <163536642+Marturelo007@users.noreply.github.com>
2024-09-04 08:46:40 -0300

    das

--- src/app/usersD/page.jsx
+++ src/app/usersD/page.jsx
@@ -4,22 +4,50 @@ import { useEffect } from "react";
 import Navigation from "@/components/navigation";
 import Users from "@/components/Users"
 import { useSocket } from "@/hooks/useSocket";
+import { Socket } from "socket.io-client";
 
 export default function UsersRanking() {
   const { socket, isConnected } = useSocket();
 
   useEffect(() => {
+    if (!socket) return;
+
+    socket.on('pingAll', (data) => {
+      console.log("Me llego el evento pingAll", data);
+    })
 
   }, [socket, isConnected]);
 
+  function handleClick() {
+    socket.emit('pingAll', {message: "hola desde mi compu"})
+  }
+  const card = {
+    borderBottom: '1px solid rgba(255, 255, 255, 0.3)',
+  }
   return(
-    <h1>
-      Soy la ruta 
-    </h1>
+<>
+<h1>HOLA Q ACE</h1>
+<button onClick={handleClick} text="enviar"></button>
+</>
+
   );
 }
 
 
+/* <body
+style={{
+  backgroundColor: `blue`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  height: '100vh',
+  width: '100vw'
+  }}>
+
+{ /*              <Navigation/>
+ <Users/>
+
+ </body> */
 
 
 
@@ -46,29 +74,3 @@ export default function UsersRanking() {
 
 
 
-
-{/* 
- const card = {
-//   borderBottom: '1px solid rgba(255, 255, 255, 0.3)',
-// }
-
-// async function IndexPage() {
-//   const users = await fetchUsers()
-//   return (
-//            <body
-//            style={{
-//            backgroundColor: `blue`,
-//            backgroundSize: 'cover',
-//            backgroundPosition: 'center',
-//            backgroundRepeat: 'no-repeat',
-//            height: '100vh',
-//            width: '100vw',
-//            }}>
-//           <Navigation />
-//           <Users users={users}/>
-          
-//           </body>
-//   );
-// }
-
-// export default IndexPage; */}
